f5862b2e1bf13b6a1d81919f7dd479a08251daaf
Parth Ghadole <146375108+ParthGhadole@users.noreply.github.com>
2025-04-17 16:39:12 +0530

    Final Changes made

--- 1.XGBoost/6.Log_Transformed_hypertuning.py
+++ 1.XGBoost/6.Log_Transformed_hypertuning.py
@@ -16,18 +16,33 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 xgb = XGBRegressor(objective='reg:squarederror', random_state=42)
 
+# param_grid = {
+#     'n_estimators': [100, 200, 300, 400],                     # Number of boosting rounds
+#     'learning_rate': [0.01, 0.05, 0.1, 0.2],                  # Step size for each boosting round
+#     'max_depth': [3, 5, 7, 9],                                  # Maximum depth of each tree
+#     'min_child_weight': [1, 3, 5, 7],                           # Minimum sum of instance weight needed in a child
+#     'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],                    # Fraction of samples used per boosting round
+#     'gamma': [0, 0.1, 0.2, 0.3, 0.4],                         # Minimum loss reduction required to make a partition
+#     'scale_pos_weight': [1, 2, 3],                             # Controls the balance of positive and negative weights
+#     'reg_alpha': [0, 0.1, 0.5, 1],                             # L1 regularization term on weights
+#     'reg_lambda': [0, 0.1, 0.5, 1],                            # L2 regularization term on weights
+#     'booster': ['gbtree', 'gblinear', 'dart'],                 # The type of boosting model
+#     'max_delta_step': [0, 1, 5], 
+#     'objective': ['reg:squarederror'],                         # Objective function
+# }
+
 param_grid = {
-    'n_estimators': [100, 200, 300, 400],                     # Number of boosting rounds
-    'learning_rate': [0.01, 0.05, 0.1, 0.2],                  # Step size for each boosting round
-    'max_depth': [3, 5, 7, 9],                                  # Maximum depth of each tree
-    'min_child_weight': [1, 3, 5, 7],                           # Minimum sum of instance weight needed in a child
-    'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],                    # Fraction of samples used per boosting round
-    'gamma': [0, 0.1, 0.2, 0.3, 0.4],                         # Minimum loss reduction required to make a partition
-    'scale_pos_weight': [1, 2, 3],                             # Controls the balance of positive and negative weights
-    'reg_alpha': [0, 0.1, 0.5, 1],                             # L1 regularization term on weights
-    'reg_lambda': [0, 0.1, 0.5, 1],                            # L2 regularization term on weights
-    'booster': ['gbtree', 'gblinear', 'dart'],                 # The type of boosting model
-    'max_delta_step': [0, 1, 5], 
+    'n_estimators': [550],                     # Number of boosting rounds
+    'learning_rate': [0.05],                  # Step size for each boosting round
+    'max_depth': [7],                                  # Maximum depth of each tree
+    'min_child_weight': [1],                           # Minimum sum of instance weight needed in a child
+    'subsample': [0.6],                    # Fraction of samples used per boosting round
+    'gamma': [0],                         # Minimum loss reduction required to make a partition
+    'scale_pos_weight': [1],                             # Controls the balance of positive and negative weights
+    'reg_alpha': [0.1],                             # L1 regularization term on weights
+    'max_delta_step': [0],                               # Maximum step size in each boosting round
+    'reg_lambda': [1],                            # L2 regularization term on weights
+    'booster': ['gbtree'],                 # The type of boosting model
     'objective': ['reg:squarederror'],                         # Objective function
 }
 
